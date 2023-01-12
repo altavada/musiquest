f336b69c0b6928a2d66ec36cfdd4b12b4f83388d
BranningK <bmk2110@gmail.com>
2023-01-11 21:28:55 -0500

    early CSS styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,15 @@
 $('#actionbutton').click(function() {
     let toggle = $('#actionbutton');
     if (toggle.attr('data-toggle') === 'on') {
-        $('#songname').text('Tennessee Whiskey');
+        $('#songname').text('Tennessee Whiskey - '); //added space and "-" for styling purposes
         $('#artistname').text('Chris Stapleton');
         $('#videoplayer').attr('src','https://www.youtube.com/embed/4zAThXFOy2c');
         $('.beforesearch').attr('style','display:none');
         $('.aftersearch').removeAttr('style');
         $('#songnamebyartist').removeAttr('style');
         toggle.text('Go Back');
+        toggle.removeClass('is-pulled-right'); //pulls the submit button back to the right
+        toggle.addClass('mt-4'); //adds more margin to the top to look better
         toggle.attr('data-toggle','off');
     } else {
         $('#songname').text('');
