c7ac5d0b42ad4fd7e691588a49e6f4566ed097aa
altavada <117209509+altavada@users.noreply.github.com>
2023-01-12 22:24:06 -0500

    YouTube fetch refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,53 +5,15 @@ let videoPlayer = $('#videoplayer');
 let preSearch = $('.beforesearch');
 let postSearch = $('.aftersearch');
 let userDash = $('#userdashboard');
+let discography = $("#discography");
 let urlDisco = "https://theaudiodb.com/api/v1/json/2/discography.php?s=";
 let urlInfo = "https://www.theaudiodb.com/api/v1/json/523532/search.php?s=";
-let youTubeVid = "https://www.youtube.com/watch?v=";
-let userDash = $("#userdashboard");
+let youTubeVid = "https://www.youtube.com/embed/";
 
 actionButton.click(userSearch);
 
 function userSearch() {
     let artistName = searchArtist.val();
-    fetch(urlDisco + artistName)
-    .then(function (response) {
-        if (response.ok) {
-            return response.json();
-        } else {
-            return Promise.reject(response);
-        }
-    })
-    .then(data => {
-        let discoArray = data.album.length;
-        for (let i = 0; i < discoArray; i++) {
-            discography.append(`<li>${data.album[i].strAlbum} -- ${data.album[i].intYearReleased}</li>`);
-        }
-    })
-    fetch(urlInfo + artistName)
-    .then(function (response) {
-        if (response.ok) {
-            return response.json();
-        } else {
-            return Promise.reject(response);
-        }
-    })
-    .then(data => {
-        userDash.prepend(`<img src=${data.artists[0].strArtistBanner}>`);
-    })
-    let youTubeID = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + artistName + "&key=AIzaSyDefkvE7bM1ACryGnTt2zai9Z-pHZGAEXo"
-    fetch(youTubeID)
-    .then(function (response) {
-        if (response.ok) {
-            return response.json();
-        } else {
-            return Promise.reject(response);
-        }
-    })
-    .then(data => {
-        discography.append(`<img class="thumbNail" src="${data.items[0].snippet.thumbnails.default.url}"> 
-            <a class="videoLink" href="https://www.youtube.com/watch?v=${data.items[0].id.videoId}" target="_blank">${data.items[0].snippet.title}</a>`);  
-})
     if (actionButton.attr('data-toggle') == 'on') {
         fetchData(artistName);
     } else {
@@ -68,7 +30,7 @@ function userSearch() {
 function fetchData(name) {
     $('#songnamebyartist').text(name);
     // this line to embed the video(s) will likely go somewhere else once YT API is integrated
-    videoPlayer.attr('src','https://www.youtube.com/embed/4zAThXFOy2c');
+    // videoPlayer.attr('src','https://www.youtube.com/embed/4zAThXFOy2c');
     fetch(urlDisco + name)
         .then(function (response) {
             if (response.ok) {
@@ -80,7 +42,7 @@ function fetchData(name) {
         .then(data => {
             let discoArray = data.album.length;
             for (let i = 0; i < discoArray; i++) {
-                $("#discography").append(`<li>${data.album[i].strAlbum} (${data.album[i].intYearReleased})</li>`);
+                discography.append(`<li>${data.album[i].strAlbum} (${data.album[i].intYearReleased})</li>`);
             }   
             preSearch.css('display', 'none');
             postSearch.removeAttr('style');
@@ -101,6 +63,21 @@ function fetchData(name) {
                 userDash.prepend('<img src=' + banner + '>');
             }
         })
+    let youTubeID = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + name + "&key=AIzaSyDefkvE7bM1ACryGnTt2zai9Z-pHZGAEXo"
+    fetch(youTubeID)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject(response);
+            }
+        })
+        .then(data => {
+            videoPlayer.attr('src', youTubeVid + data.items[0].id.videoId);
+            discography.append(`<img class="thumbNail" src="${data.items[0].snippet.thumbnails.default.url}">`);
+            // discography.append(`<img class="thumbNail" src="${data.items[0].snippet.thumbnails.default.url}">
+            // <a class="videoLink" href="https://www.youtube.com/watch?v=${data.items[0].id.videoId}" target="_blank">${data.items[0].snippet.title}</a>`);  
+})
 }
 
 
