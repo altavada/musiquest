eebf56bf2d956222aa0ee8c02e3dda1cad9ea5c9
altavada <117209509+altavada@users.noreply.github.com>
2023-01-12 23:18:57 -0500

    function-wrapping new code for clean working priorities

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,27 +43,31 @@ function fetchData(name) {
             let discoArray = data.album.length;
             for (let i = 0; i < discoArray; i++) {
                 discography.append(`<li>${data.album[i].strAlbum} (${data.album[i].intYearReleased})</li>`);
-            }   
+            }
+            fetchMedia(name);   
             preSearch.css('display', 'none');
             postSearch.removeAttr('style');
             actionButton.text('Go Back');
             actionButton.attr('data-toggle','off');
         })
-    fetch(urlInfo + name)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            } else {
-                return Promise.reject(response);
-            }
-        })
-        .then(data => {
-            let banner = data.artists[0].strArtistBanner;
-            if (banner != null) {
-                userDash.prepend('<img src=' + banner + '>');
-            }
-        })
-    let youTubeID = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + name + "&key=AIzaSyDefkvE7bM1ACryGnTt2zai9Z-pHZGAEXo"
+}
+
+function fetchMedia(artist) {
+    fetch(urlInfo + artist)
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    })
+    .then(data => {
+        let banner = data.artists[0].strArtistBanner;
+        if (banner != null) {
+            userDash.prepend('<img src=' + banner + '>');
+        }
+    })
+    let youTubeID = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + artist + "&key=AIzaSyDefkvE7bM1ACryGnTt2zai9Z-pHZGAEXo"
     fetch(youTubeID)
         .then(function (response) {
             if (response.ok) {
@@ -77,9 +81,8 @@ function fetchData(name) {
             discography.append(`<img class="thumbNail" src="${data.items[0].snippet.thumbnails.default.url}">`);
             // discography.append(`<img class="thumbNail" src="${data.items[0].snippet.thumbnails.default.url}">
             // <a class="videoLink" href="https://www.youtube.com/watch?v=${data.items[0].id.videoId}" target="_blank">${data.items[0].snippet.title}</a>`);  
-})
+    })
 }
-
 
 
 // https://theaudiodb.com/api/v1/json/2/discography.php?s=
