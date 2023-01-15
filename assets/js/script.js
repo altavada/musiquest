7cb83e27449b8fbea6c76b2554ee44623a8332cb
Branning <bmk2110@gmail.com>
2023-01-15 10:52:55 -0500

    added banner to page, added function to return to home page on h1 click

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,25 @@ let searchArtist = $("#searchlyrics");
 let videoPlayer = $('#videoplayer');
 let preSearch = $('.beforesearch');
 let postSearch = $('.aftersearch');
+let wrapperLyrics = $('#songnamebyartist');
 let userDash = $('#userdashboard');
 let discography = $("#discography");
+let musiQuest = $('#headline')
 let urlDisco = "https://theaudiodb.com/api/v1/json/2/discography.php?s=";
 let urlInfo = "https://www.theaudiodb.com/api/v1/json/523532/search.php?s=";
 let youTubeVid = "https://www.youtube.com/embed/";
 
 actionButton.click(userSearch);
+musiQuest.click(headingRefresh); //Event listener for click on h1 (MusiQuest)
+
+//Function to ensure it only runs after a search has been completed
+function headingRefresh(){
+    if (actionButton.attr('data-toggle') == 'off') {
+        userSearch();
+    } else {
+        return;
+    }
+}
 
 function userSearch() {
     let artistName = searchArtist.val();
@@ -28,7 +40,7 @@ function userSearch() {
 }
 
 function fetchData(name) {
-    $('#songnamebyartist').text(name);
+    // $('#songnamebyartist').text(name);
     fetch(urlDisco + name)
         .then(function (response) {
             if (response.ok) {
@@ -59,19 +71,10 @@ function fetchMedia(artist) {
             return Promise.reject(response);
         }
     })
-
-    fetch(urlInfo + artistName)
-    .then(function (response) {
-        if (response.ok) {
-            return response.json();
-        } else {
-            return Promise.reject(response);
-        }
-    })
     .then(data => {
         let banner = data.artists[0].strArtistBanner;
         if (banner != null) {
-            userDash.prepend('<img src=' + banner + '>');
+            wrapperLyrics.prepend('<img src=' + banner + '>');
         }
     })
     let youTubeID = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + artist + "&key=AIzaSyDefkvE7bM1ACryGnTt2zai9Z-pHZGAEXo"
@@ -91,65 +94,14 @@ function fetchMedia(artist) {
                 x++;
             }
             videoPlayer.attr('src', youTubeVid + data.items[x].id.videoId);
-            discography.append(`<img class="thumbNail" src="${data.items[x].snippet.thumbnails.default.url}">
-            <a class="videoLink" href="https://www.youtube.com/watch?v=${data.items[x].id.videoId}" target="_blank">${data.items[x].snippet.title}</a>`);  
+            // discography.append(`<img class="thumbNail" src="${data.items[x].snippet.thumbnails.default.url}">
+            // <a class="videoLink" href="https://www.youtube.com/watch?v=${data.items[x].id.videoId}" target="_blank">${data.items[x].snippet.title}</a>`);  
     })
-    .then(data => {
-        discography.append(`<img class="thumbNail" src="${data.items[0].snippet.thumbnails.default.url}"> 
-            <a class="videoLink" href="https://www.youtube.com/watch?v=${data.items[0].id.videoId}" target="_blank">${data.items[0].snippet.title}</a>`);  
-})
-    if (actionButton.attr('data-toggle') == 'on') {
-        fetchData(artistName);
-    } else {
-        $(".result-field").text('');
-        videoPlayer.removeAttr('src');
-        preSearch.removeAttr('style');
-        postSearch.css('display', 'none');
-        userDash.children('img').remove();
-        actionButton.text('Submit');
-        actionButton.attr('data-toggle','on');
-    }
-}
 }
 
-function fetchData(name) {
-    $('#songnamebyartist').text(name);
-    // this line to embed the video(s) will likely go somewhere else once YT API is integrated
-    videoPlayer.attr('src','https://www.youtube.com/embed/4zAThXFOy2c');
-    fetch(urlDisco + name)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            } else {
-                return Promise.reject(response);
-            }
-        })
-        .then(data => {
-            let discoArray = data.album.length;
-            for (let i = 0; i < discoArray; i++) {
-                $("#discography").append(`<li class="mt-2 ml-5 is-size-5">${data.album[i].strAlbum} (${data.album[i].intYearReleased})</li>`);
-            }   
-            preSearch.css('display', 'none');
-            postSearch.removeAttr('style');
-            actionButton.text('Go Back');
-            actionButton.attr('data-toggle','off');
-        })
-    fetch(urlInfo + name)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            } else {
-                return Promise.reject(response);
-            }
-        })
-        // .then(data => {
-        //     let banner = data.artists[0].strArtistBanner;
-        //     // if (banner != null) {
-        //     //     userDash.prepend('<img src=' + banner + '>');
-        //     // }
-        // })
+function embedVideo() {
+    
 }
 
 // https://theaudiodb.com/api/v1/json/2/discography.php?s=
 // Artist Albums & Release Years
-
