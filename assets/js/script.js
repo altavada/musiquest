970e1a4125b0e5b041ee48ae0e7d2ad7a9bf7894
BranningK <bmk2110@gmail.com>
2023-01-17 23:23:28 -0500

    added link to names in footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,107 @@
-// DEMO ONLY, REMOVE/REWRITE WHEN DEVELOPING JS -- attaches display-toggle to button to demonstrate before/after search-event states
-$('#actionbutton').click(function() {
-    let toggle = $('#actionbutton');
-    if (toggle.attr('data-toggle') === 'on') {
-        $('#songname').text('Tennessee Whiskey - '); //added space and "-" for styling purposes
-        $('#artistname').text('Chris Stapleton');
-        $('#videoplayer').attr('src','https://www.youtube.com/embed/4zAThXFOy2c');
-        $('.beforesearch').attr('style','display:none');
-        $('.aftersearch').removeAttr('style');
-        $('#songnamebyartist').removeAttr('style');
-        toggle.text('Go Back');
-        toggle.removeClass('is-pulled-right'); //pulls the submit button back to the right
-        toggle.addClass('mt-4'); //adds more margin to the top to look better
-        toggle.attr('data-toggle','off');
+
+let actionButton = $("#actionbutton");
+let searchArtist = $("#searchlyrics");
+let videoPlayer = $('#videoplayer');
+let preSearch = $('.beforesearch');
+let postSearch = $('.aftersearch');
+let wrapperLyrics = $('#songnamebyartist');
+let userDash = $('#userdashboard');
+let discography = $("#discography");
+let musiQuest = $('#headline')
+let urlDisco = "https://theaudiodb.com/api/v1/json/2/discography.php?s=";
+let urlInfo = "https://www.theaudiodb.com/api/v1/json/523532/search.php?s=";
+let youTubeVid = "https://www.youtube.com/embed/";
+
+actionButton.click(userSearch);
+musiQuest.click(headingRefresh); //Event listener for click on h1 (MusiQuest)
+
+//Function to ensure it only runs after a search has been completed
+function headingRefresh(){
+    if (actionButton.attr('data-toggle') == 'off') {
+        userSearch();
     } else {
-        $('#songname').text('');
-        $('#artistname').text('');
-        $('#videoplayer').removeAttr('src');
-        $('.beforesearch').removeAttr('style');
-        $('.aftersearch').attr('style','display:none');
-        $('#songnamebyartist').attr('style', 'display:none');
-        toggle.text('Submit');
-        toggle.attr('data-toggle','on');
+        return;
     }
-})
+}
+
+function userSearch() {
+    let artistName = searchArtist.val();
+    if (actionButton.attr('data-toggle') == 'on') {
+        fetchData(artistName);
+    } else {
+        $(".result-field").text('');
+        videoPlayer.removeAttr('src');
+        preSearch.removeAttr('style');
+        postSearch.css('display', 'none');
+        userDash.children('img').remove();
+        actionButton.text('Submit');
+        actionButton.attr('data-toggle','on');
+    }
+}
+
+function fetchData(name) {
+    // $('#songnamebyartist').text(name);
+    fetch(urlDisco + name)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject(response);
+            }
+        })
+        .then(data => {
+            let discoArray = data.album.length;
+            for (let i = 0; i < discoArray; i++) {
+                discography.append(`<li>${data.album[i].strAlbum} (${data.album[i].intYearReleased})</li>`);
+            }
+            fetchMedia(name);   
+            preSearch.css('display', 'none');
+            postSearch.removeAttr('style');
+            actionButton.text('Go Back');
+            actionButton.attr('data-toggle','off');
+        })
+}
+
+function fetchMedia(artist) {
+    fetch(urlInfo + artist)
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    })
+    .then(data => {
+        let banner = data.artists[0].strArtistBanner;
+        if (banner != null) {
+            wrapperLyrics.prepend('<img src=' + banner + '>');
+        }
+    })
+    let youTubeID = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + artist + "&key=AIzaSyDefkvE7bM1ACryGnTt2zai9Z-pHZGAEXo"
+    fetch(youTubeID)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject(response);
+            }
+        })
+        .then(data => {
+            console.log(data);
+            // debug for first search result sometimes being a YT channel instead of a video
+            let x = 0;
+            if (data.items[0].id.kind != 'youtube#video') {
+                x++;
+            }
+            videoPlayer.attr('src', youTubeVid + data.items[x].id.videoId);
+            // discography.append(`<img class="thumbNail" src="${data.items[x].snippet.thumbnails.default.url}">
+            // <a class="videoLink" href="https://www.youtube.com/watch?v=${data.items[x].id.videoId}" target="_blank">${data.items[x].snippet.title}</a>`);  
+    })
+}
+
+function embedVideo() {
+    
+}
+
+// https://theaudiodb.com/api/v1/json/2/discography.php?s=
+// Artist Albums & Release Years
