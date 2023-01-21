89febff84fdfe9c22c5e1acea5917ce8362db278
Veis1337 <117036853+Veis1337@users.noreply.github.com>
2023-01-21 16:56:59 -0500

    history

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,40 +11,61 @@ let musiQuest = $('#headline')
 let urlDisco = "https://theaudiodb.com/api/v1/json/2/discography.php?s=";
 let urlInfo = "https://www.theaudiodb.com/api/v1/json/523532/search.php?s=";
 let youTubeVid = "https://www.youtube.com/embed/";
+let history = $("#history");
+let historyCount = localStorage.length;
 
 actionButton.click(userSearch);
 musiQuest.click(headingRefresh); //Event listener for click on h1 (MusiQuest)
 window.addEventListener('load', (event) => {
-    new cursoreffects.springyEmojiCursor({emoji: "🎤🎵"});
-  });
+    new cursoreffects.springyEmojiCursor({ emoji: "🎤🎵" });
+});
+
+$(document).on("click", ".historyLi", function () {
+    searchArtist.val($(this).text());
+    userSearch();
+});
+
+startUp();
+function startUp() {
+    Object.keys(localStorage).forEach(function (key) {
+        history.append(`<li class="historyLi">${localStorage.getItem(key)}</li>`);
+        if ($("#history li").length > 4) {
+            $("#history li").last().remove();
+        }
+    });
+}
 
 //Function to ensure it only runs after a search has been completed
-function headingRefresh(){
+function headingRefresh() {
     if (actionButton.attr('data-toggle') == 'off') {
+        startUp();
         userSearch();
     } else {
+        startUp();
         return;
     }
 }
 
 function userSearch() {
     let artistName = searchArtist.val();
+    if (!historyCount) {
+        historyCount = 0;
+    } else {
+        historyCount = historyCount + 1;
+    }
     if (actionButton.attr('data-toggle') == 'on') {
+        localStorage.setItem("history" + historyCount, artistName);
         fetchData(artistName);
     } else {
-        $(".result-field").text('');
-        videoPlayer.removeAttr('src');
-        preSearch.removeAttr('style');
-        postSearch.css('display', 'none');
-        userDash.children('img').remove();
-        actionButton.text('Submit');
-        actionButton.attr('data-toggle','on');
         location.reload();
+    }
+    if ($("#history li").length > 4) {
+        $("#history li").last().remove();
     }
 }
 
 function fetchData(name) {
-    
+
     fetch(urlDisco + name)
         .then(function (response) {
             if (response.ok) {
@@ -56,7 +77,7 @@ function fetchData(name) {
         .then(data => {
             if (data.album == null) {
                 searchArtist.val('');
-                searchArtist.attr("placeholder","Artist Not Found -- Please try again");
+                searchArtist.attr("placeholder", "Artist Not Found -- Please try again");
                 return;
             } else {
                 let discoArray = data.album.length;
@@ -67,28 +88,28 @@ function fetchData(name) {
                 preSearch.css('display', 'none');
                 postSearch.removeAttr('style');
                 actionButton.text('Go Back');
-                actionButton.attr('data-toggle','off');
+                actionButton.attr('data-toggle', 'off');
             }
         })
 }
 
 function fetchMedia(artist) {
     fetch(urlInfo + artist)
-    .then(function (response) {
-        if (response.ok) {
-            return response.json();
-        } else {
-            return Promise.reject(response);
-        }
-    })
-    .then(data => {
-        let banner = data.artists[0].strArtistBanner;
-        if (banner != null) {
-            songNBA.prepend('<img src=' + banner + '>');
-        } else {
-            $('#songnamebyartist').text(data.artists[0].strArtist);
-        }
-    })
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject(response);
+            }
+        })
+        .then(data => {
+            let banner = data.artists[0].strArtistBanner;
+            if (banner != null) {
+                songNBA.prepend('<img src=' + banner + '>');
+            } else {
+                $('#songnamebyartist').text(data.artists[0].strArtist);
+            }
+        })
     let youTubeID = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + artist + "&key=AIzaSyDefkvE7bM1ACryGnTt2zai9Z-pHZGAEXo"
     fetch(youTubeID)
         .then(function (response) {
@@ -108,11 +129,11 @@ function fetchMedia(artist) {
             videoPlayer.attr('src', youTubeVid + data.items[x].id.videoId);
             // discography.append(`<img class="thumbNail" src="${data.items[x].snippet.thumbnails.default.url}">
             // <a class="videoLink" href="https://www.youtube.com/watch?v=${data.items[x].id.videoId}" target="_blank">${data.items[x].snippet.title}</a>`);  
-    })
+        })
 }
 
 function embedVideo() {
-    
+
 }
 
 // https://theaudiodb.com/api/v1/json/2/discography.php?s=
